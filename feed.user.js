3f24afa6ce245e9ab146c3cdd963bebc0862baec
Firedrake969 <Firedrake969@users.noreply.github.com>
2014-10-14 17:18:02 -0600

    Update feed.user.js

--- feed.user.js
+++ feed.user.js
@@ -38,9 +38,18 @@ if ($.inArray('manager', hide) > -1) {
   $( "#activity-feed li:contains('was promoted to manager of')" ).css( "display", "none" );
 }
 
-$(".box-head h4").append( "<span class='notice'>Some messages hidden</span>" )
+$("#whats-happenin .box-head").append( "<span class='notice'>Some messages hidden</span>" )
 
-$(".notice").css({"color":"#aaa", "font-size":"12px", "text-shadow":"none", "font-weight":"400", "line-height":"0", "float":"right", "margin-top":"13px", "cursor":"pointer"});
+$(".notice").css({
+  "color":"#aaa",
+  "font-size":"12px",
+  "text-shadow":"none",
+  "font-weight":"400",
+  "line-height":"0", "float":"right",
+  "margin-top":"13px",
+  "cursor":"pointer",
+  "right":"0px"
+});
 
 $(".notice").click(function() {
   $( "#activity-feed li" ).css( "display", "list-item" );
