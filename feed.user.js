1917c711abd7ffcb4cbfd037f1b1f98422e8cb0b
Firedrake969 <Firedrake969@users.noreply.github.com>
2014-10-14 17:13:19 -0600

    Create feed.user.js

---
+++ feed.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name		Scratch Feed
+// @include		http://scratch.mit.edu/
+// @version		0.1
+// @grant		none
+// ==/UserScript==
+
+//great thanks to PullJosh's MessageFilter.user.js for guidance.
+
+//"love", "share", "favorite", "remix", "following", "curator", "manager"
+var hide = ["remix", "following", "curator", "manager"];
+
+if ($.inArray('love', hide) > -1) {
+  $( "#activity-feed li div:contains('loved')" ).css( "display", "none" );
+}
+
+if ($.inArray('share', hide) > -1) {
+  $( "#activity-feed li:contains('shared the project')" ).css( "display", "none" );
+}
+
+if ($.inArray('favorite', hide) > -1) {
+  $( "#activity-feed li:contains('favorited')" ).css( "display", "none" );
+}
+
+if ($.inArray('remix', hide) > -1) {
+  $( "#activity-feed li:contains('remixed')" ).css( "display", "none" );
+}
+
+if ($.inArray('following', hide) > -1) {
+  $( "#activity-feed li:contains('is now following')" ).css( "display", "none" );
+}
+
+if ($.inArray('curator', hide) > -1) {
+  $( "#activity-feed li:contains('became a curator of')" ).css( "display", "none" );
+}
+
+if ($.inArray('manager', hide) > -1) {
+  $( "#activity-feed li:contains('was promoted to manager of')" ).css( "display", "none" );
+}
