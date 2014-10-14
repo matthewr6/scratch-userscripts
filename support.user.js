d9a4e4119ce98677fa6241dfc418b4acd50e0335
Firedrake969 <Firedrake969@users.noreply.github.com>
2014-10-14 17:56:46 -0600

    Update support.user.js

--- support.user.js
+++ support.user.js
@@ -5,9 +5,11 @@
 // @grant		none
 // ==/UserScript==
 
-var textcontent = "<form><input type='radio' name='choice' value='support' />&nbspYes<br /><br /><input type='radio' name='choice' value='nosupport' />&nbspNo<br /><br /><textarea id='comments' rows='1' cols='100'></textarea><br /><br /><input type='button' value='Submit' id='submitsupport'></input></form>";
+var textcontent = "<form><input type='radio' name='choice' value='support' />&nbspYes<br /><br />Comments or reason?<input type='radio' name='choice' value='nosupport' />&nbspNo<br /><br /><textarea id='comments' rows='1' cols='100'></textarea><br /><br /><input type='button' value='Submit' id='submitsupport'></input></form>";
 var boxhead = "<div class='box text-center'><div class='boxhead support-check'><h2>Do you support?</h2></div>";
 var boxcontent = "<div class='box-content'>" + textcontent + "</div></div>";
+
+var feedback;
 
 $('#brdheader').after(boxhead + boxcontent);
 
@@ -17,3 +19,12 @@ $('.support-check').css({
 });
 
 $('.text-center').css('text-align', 'center');
+
+$('#submitsupport').click(function() {
+    if ($("input[name=choice]:checked").val() == 'support') {
+        feedback = "Support.  ";
+    } else if ($("input[name=choice]:checked").val() == 'nosupport') {
+        feedback = "No support.  ";
+    }
+    feedback += $('#comments').val();
+});
